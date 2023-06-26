dbfed6f795a59d798ae1eca9fff17b6065a1f336
Mailson Teles Borges <mailson.borges@projuris.com.br>
2023-06-26 18:49:32 -0500

    Remove coments

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -4,14 +4,7 @@
  * ------------------------------------------------------------------------------------------ */
 
 import * as path from 'path';
-import {
-    workspace,
-    ExtensionContext,
-    languages,
-    Definition,
-    Position,
-    Range,
-} from 'vscode';
+import { workspace, ExtensionContext } from 'vscode';
 
 import {
     LanguageClient,
@@ -23,38 +16,32 @@ import {
 let client: LanguageClient;
 
 export function activate(context: ExtensionContext) {
-    // The server is implemented in node
     const serverModule = context.asAbsolutePath(path.join('dist', 'server.js'));
 
-    // If the extension is launched in debug mode then the debug server options are used
-    // Otherwise the run options are used
+    const debugOptions = { execArgv: ['--nolazy', '--inspect=6009'] };
+
     const serverOptions: ServerOptions = {
         run: { module: serverModule, transport: TransportKind.ipc },
         debug: {
             module: serverModule,
             transport: TransportKind.ipc,
+            options: debugOptions,
         },
     };
 
-    // Options to control the language client
     const clientOptions: LanguageClientOptions = {
-        // Register the server for plain text documents
         documentSelector: [{ scheme: 'file', language: 'jabuti' }],
         synchronize: {
-            // Notify the server about file changes to '.clientrc files contained in the workspace
             fileEvents: workspace.createFileSystemWatcher('**/.clientrc'),
         },
     };
 
-    // Create the language client and start the client.
     client = new LanguageClient(
-        'jabutiLSPServer',
         'Language Server Jabuti',
         serverOptions,
         clientOptions,
     );
 
-    // Start the client. This will also launch the server
     client.start();
 }
 
